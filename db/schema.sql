d3039b91091d9c111813c3e2d4daf1bd96057aa9
Nolan <nolannaphys@gmail.com>
2023-08-21 17:59:01 -0400

    Updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,28 +3,32 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE departments (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
+CREATE TABLE role (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
-    FOREIGN KEY (department_id)
-        REFERENCES department(id) ON DELETE SET NULL
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE SET NULL
 );
 
-CREATE TABLE employees (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT NOT NULL,
+    role_id INT,
+    manager_id INT,
     FOREIGN KEY (role_id)
-        REFERENCES role(id) ON DELETE SET NULL,
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+    PRIMARY KEY (id),
     FOREIGN KEY (manager_id)
-        REFERENCES employee(id) ON DELETE SET NULL
+    REFERENCES employee(id)
+    ON DELETE SET NULL
 );
